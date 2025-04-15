107a0c27bdf85b518a0c1d7add6666c8abc4d4e2
Matheus <ra23371012-2@aluno.unicesumar.edu.br>
2025-04-15 15:49:27 -0300

    Estruturando o teste de login invalido

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,9 +9,13 @@ class LoginPage(BasePage):
         self.driver = conftest.driver
         self.username_field = (By.ID, "user-name")
         self.password_field = (By.ID, "password")
-        self.login_button_field = (By.ID, "login-button")
+        self.login_button = (By.ID, "login-button")
+        self.error_login_message = (By.XPATH, "//*[@data-test='error']")
 
     def fazer_login(self, usuario, senha):
         self.escrever(self.username_field, usuario)
         self.escrever(self.password_field, senha)
-        self.clicar(self.login_button_field)
+        self.clicar(self.login_button)
+
+    def verificar_mensagem_erro_login(self):
+        self.verificar_elemento_existente(self.error_login_message)
--- tests/test_ct02_login_invalido.py
+++ tests/test_ct02_login_invalido.py
@@ -1,13 +1,20 @@
+import time
+
 import pytest
-from selenium.webdriver.common.by import By
-import conftest
+
+from pages.login_page import LoginPage
+
 
 @pytest.mark.usefixtures("setup_teardown" )
 class TestCT02:
     def test_ct02_login_invalido(self):
-        driver = conftest.driver
 
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("teste")
-        driver.find_element(By.ID, "login-button").click()
-        assert driver.find_element(By.XPATH, "//span[@class='title']").is_displayed()
+        # Instancia os objetos a serem usados no teste
+        login_page = LoginPage()
+
+        # Faz login
+        login_page.fazer_login("standard_user", "error123")
+
+        # Verificar se o login não foi realizado e se a mensagem de erro apareceu
+        login_page.verificar_mensagem_erro_login()
+        time.sleep(3)
